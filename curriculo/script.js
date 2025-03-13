19fd5b2f8875b0e64e4b7bb999ed435d920d1c41
Isaias Rocha <119088042+isaiasrocha01@users.noreply.github.com>
2025-03-13 15:33:07 +0000

    Mensagem do commit

--- curriculo/script.js
+++ curriculo/script.js
@@ -1,1 +1,10 @@
 alert('Olá! Para fazer a impressão selecione a Escala 58');
+alert('Olá! Para fazer a impressão selecione a Escala 58');
+document.getElementById("titulo-educacao").addEventListener("click", function() {
+    const educacaoContainer = document.getElementById("educacao-container");
+    if (educacaoContainer.style.display === "none" || educacaoContainer.style.display === "") {
+        educacaoContainer.style.display = "block";
+    } else {
+        educacaoContainer.style.display = "none";
+    }
+});
